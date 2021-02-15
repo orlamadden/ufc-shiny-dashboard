30aa08078368367d8b76228d0d606110935104d0
orlamadden <omadden2@gmail.com>
2021-02-15 19:38:53 +0000

    added dashboard initial layout and title

--- app.R
+++ app.R
@@ -8,27 +8,11 @@ df <- read_csv("https://raw.githubusercontent.com/andrew-couch/Tidy-Tuesday/mast
 
 elo_df <- read_csv("https://raw.githubusercontent.com/andrew-couch/Tidy-Tuesday/master/TidyTuesdayUFCDashboard/elo_df.csv")
 
-
-ui <- fluidPage(
+ui <- dashboardPage(
+    dashboardHeader(title = "UFC Dashboard"),
+    dashboardSidebar(),
+    dashboardBody()
     
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-    
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-        
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
-    )
 )
 
 # Define server logic required to draw a histogram
